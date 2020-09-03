75c5271f4c4951f21654806d23bf8f2b68d3b1f7
praveen <mailmeonlypraveen@gmail.com>
2020-09-03 18:40:59 +0530

    Making client ready for Heroku deployment

--- index.js
+++ index.js
@@ -39,5 +39,19 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if(process.env.NODE_ENV === "production")
+{
+    //Express will serve up production assests
+    //like our main.js file, or main.css file
+    app.use(express.static('client/build'));
+
+    //Express will serve up index.html if it doesnt recognize the path
+    const path = require('path');
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+    });
+}
+
+
 const PORT = process.env.PORT || 5000 //process.env.PORT will be set automatically by Heroku server
 app.listen(PORT);
