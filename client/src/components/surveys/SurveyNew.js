6fa87ca25e369b090f4fe398cbfb815e2ed6ecea
praveen <mailmeonlypraveen@gmail.com>
2020-09-07 19:50:58 +0530

    clearn survey form on component unload

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -2,6 +2,7 @@
 import React, {Component} from 'react';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
+import {reduxForm } from 'redux-form';
 
 class SurveyNew extends Component{
 
@@ -24,4 +25,13 @@ class SurveyNew extends Component{
 
 }
 
-export default SurveyNew;
+/* This is the parent component which shows one of two child component. if we switch between the child components, 
+we will retain the form value, because in surveyForm.js we made destroyOnUnmount=false.
+
+Here, if we dismount this parent component, we want the value to be destroyed, which is default behaviour.
+*/
+export default reduxForm(
+    {
+        form: 'surveyForm'
+    }
+)(SurveyNew);
